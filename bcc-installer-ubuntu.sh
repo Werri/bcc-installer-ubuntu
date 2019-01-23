a908bc54009c068b6338eec43369d7c571c92bbc
Werri <Werri@werri.com>
2019-01-23 17:26:56 +0000

    first

---
+++ bcc-installer-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
+echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+sudo apt-get update
+sudo apt-get install bcc-tools libbcc-examples linux-headers-$(uname -r)
